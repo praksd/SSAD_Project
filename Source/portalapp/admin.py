831f5d56a005dc5c37035e39846a4cfd4bbd472f
Prakrati Dangarh <prakrati.dangarh@research.iiit.ac.in>
2016-09-22 18:54:59 +0000

    Update admin.py

--- Source/portalapp/admin.py
+++ Source/portalapp/admin.py
@@ -1,12 +1,3 @@
-from django.contrib import admin
-from .models import Album, Photo
-
-class AlbumAdmin(admin.ModelAdmin):
-    list_display = ["name", "timestamp"]
-    class Meta:
-        model = Album
-
-admin.site.register(Album, AlbumAdmin)
 
 
 
