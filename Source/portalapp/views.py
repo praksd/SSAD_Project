efb7fbd4dbb447f76224211c0bb9aa47a6405683
Prakrati Dangarh <prakrati.dangarh@research.iiit.ac.in>
2016-09-25 21:36:07 +0000

    Update views.py

--- Source/portalapp/views.py
+++ Source/portalapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Photo
+
+def photo_list(request):
+    queryset = Photo.objects.all()
+    context = { 
+            "photos" : queryset,
+            }
+    return render(request, "templates", context)
+~                      
