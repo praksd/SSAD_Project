4974b05f9d03e8d5b38bec872e8a1c0d81fe85d3
Prakrati Dangarh <prakrati.dangarh@research.iiit.ac.in>
2016-09-25 21:30:31 +0000

    register Photo from models.py in admin.py

--- Source/portalapp/admin.py
+++ Source/portalapp/admin.py
@@ -1,3 +1,10 @@
 
+from django.contrib import admin
+from .models import Photo
 
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date')
+    #prepopulated_fields = { 'slug': ['title'] }
 
+admin.site.register(Photo, PhotoAdmin)
+~                                    
