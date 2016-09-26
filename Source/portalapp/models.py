219c95fd2d4ab9cd667cacddebde875591961df3
MaverickMe4556 <fireball4556@gmail.com>
2016-09-26 23:04:35 +0530

    added pressrelease class in models.py

--- Source/portalapp/models.py
+++ Source/portalapp/models.py
@@ -1,15 +1,18 @@
-from __future__ import unicode_literals 
+from __future__ import unicode_literals
+from __future__ import unicode_literals
 import datetime, os, sys
 from PIL import Image
-#from __future__ import unicode_literals 
 from django.db import models
 from django.core.files import File
 from django.conf import settings
-#from markdown import markdown
+from markdown import markdown
 from django.utils import timezone
 from datetime import datetime
+from django.db import models
+from tinymce.models import HTMLField
 
- 
+
+
 class Photo(models.Model):
         title = models.CharField(max_length=250,
                 help_text='Maximum 250 characters.', blank=True)
@@ -28,40 +31,31 @@ class Photo(models.Model):
         def __unicode__(self):
                 return self.title
 
+        def save(self, force_insert=False, force_update=False):
+                if self.summary:
+                        self.summary_html = markdown(self.summary)
+                super(Photo, self).save(force_insert, force_update)
+
+                if self.image and not self.thumb:
+                        #Set the thumbnail size and maximumsize
+                        t_size = 200, 150
+                        max_size = 800, 600
+                        # Open the image that was uploaded.
+                        im = Image.open(settings.MEDIA_ROOT + str(self.image))
+                        # Compare the image size against the maximum size. If it is greater, the image will be resized.
 
         def get_absolute_url(self):
                 return ('thaddeus_photo_detail', (),
                                 { 'slug': self.slug })
-        get_absolute_url = models.permalink(get_absolute_url)
 
 
-        """   def save(self, force_insert=False, force_update=False):
-                if self.summary:
-                    self.summary_html = markdown(self.summary)
-                    super(Photo, self).save(force_insert, force_update)
+class Album(models.Model):
+    name = models.CharField(max_length=128)
+    date_created = models.DateTimeField(auto_now_add=True, auto_now=False)
 
-                if self.image and not self.thumb:
-                    #Set the thumbnail size and maximumsize
-                    t_size = 200, 150    
-                    max_size = 800, 600
-                    # Open the image that was uploaded.
-                    im = Image.open(settings.MEDIA_ROOT + str(self.image)"""
 
-        
 
-    
-
-class Register(models.Model):
-        author = models.ForeignKey('auth.User')
-        topic = models.CharField(max_length=200)
-        abstract = models.TextField()
-        created_date = models.DateTimeField(default=timezone.now)
-        speaker = models.CharField(max_length=200)
-        venue = models.CharField(max_length=200)
-        date_and_time = models.DateTimeField(blank = False,default=datetime.now)
-        bio = models.TextField()
-        coordinator = models.CharField(max_length=200)
-
-        def __str__(self):
-            return self.speaker
-
+class PressRelease(models.Model):
+    Title = models.CharField(max_length=256)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    content = HTMLField()
