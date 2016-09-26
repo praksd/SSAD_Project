e7d3100b8aba1a69a7b36e6e57d37d3476db6a5f
simran <singhalsimran0@gmail.com>
2016-09-26 14:12:56 +0000

    admin register for Talk Registration

--- Source/portalapp/admin.py
+++ Source/portalapp/admin.py
@@ -1,5 +1,5 @@
-
 from django.contrib import admin
+from .models import Register
 from .models import Photo
 
 class PhotoAdmin(admin.ModelAdmin):
@@ -7,4 +7,9 @@ class PhotoAdmin(admin.ModelAdmin):
     #prepopulated_fields = { 'slug': ['title'] }
 
 admin.site.register(Photo, PhotoAdmin)
-~                                    
+
+
+
+admin.site.register(Register)
+
+                                    
